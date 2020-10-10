b55b1b22a0b3df17fb7ad43a0c4958b6c087b131
iaman0004 <iaman0004@users.noreply.github.com>
2020-10-11 00:47:11 +0530

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
 		// See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
 		siteTitleAlt: `Aman Kumar Sharma`,
 	},
+	pathPrefix: '/me',
 	plugins: [
 		{
 			resolve: `@lekoarts/gatsby-theme-cara`,
